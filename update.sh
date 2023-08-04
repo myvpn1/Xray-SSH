84e468a1f7e74558ed0574034a594fbcf2ff1ae0
myvpn1 <137178860+myvpn1@users.noreply.github.com>
2023-08-04 21:53:31 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 LocalVersion=$(cat /root/versi)
-Version=$(curl -sS https://raw.githubusercontent.com/andristji/Xray-SSH/main/update/versinew | awk '{print $1}')
+Version=$(curl -sS https://raw.githubusercontent.com/myvpn1/Xray-SSH/main/update/versinew | awk '{print $1}')
 if [ $LocalVersion = $Version ]; then
 echo -e "\e[32mScript Sudah Versi Terbaru...\e[0m"
 else
 echo -e "\e[32mAda Update Script!!\e[0m";
-wget -q -O updateyes.sh https://raw.githubusercontent.com/andristji/Xray-SSH/main/update/updateyes.sh && chmod +x updateyes.sh && ./updateyes.sh
+wget -q -O updateyes.sh https://raw.githubusercontent.com/myvpn1/Xray-SSH/main/update/updateyes.sh && chmod +x updateyes.sh && ./updateyes.sh
 rm -f updateyes.sh
 fi
